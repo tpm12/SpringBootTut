f73b2ad851e2e3a9f01360c0adbb9e0f5bf5274e
tpm12 <69420minecraftboy@gmail.com>
2025-02-28 18:15:25 +0100

    Feat: added RunController and RunRepository with first functionalities

---
+++ src/main/java/dev/tpm/SpringbootTut/repo/RunRepository.java
@@ -0,0 +1,44 @@
+package dev.tpm.SpringbootTut.repo;
+
+import dev.tpm.SpringbootTut.models.Location;
+import dev.tpm.SpringbootTut.models.Run;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    public List<Run> findAll(){
+        return runs;
+    }
+
+    public Run findById(int id){
+        return runs.stream().filter(run -> run.getId() == id).findFirst().orElse(null);
+    }
+
+    @PostConstruct
+    private void init(){
+        runs.add(new Run(1,
+                "Monday Morning run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(60),
+                2,
+                Location.OUTSIDE
+        ));
+        runs.add(new Run(2,
+                "Tuesday Noon run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(60),
+                4,
+                Location.INSIDE
+        ));
+    }
+
+}
