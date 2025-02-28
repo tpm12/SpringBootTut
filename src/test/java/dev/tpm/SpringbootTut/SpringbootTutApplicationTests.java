c2c5235c1d63e5084076b5ede7b8aa284efb14f3
tpm12 <69420minecraftboy@gmail.com>
2025-02-28 11:22:21 +0100

    Initial Commit

---
+++ src/test/java/dev/tpm/SpringbootTut/SpringbootTutApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tpm.SpringbootTut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
